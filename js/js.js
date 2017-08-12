edb060001ba1fbc84018b8e90d378e5c744aac2a
Алексей Хайдин <lexanic37@gmail.com>
2017-08-12 18:10:05 +0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,6 @@
+$('#fullpage').fullpage({anchors:['onePage', 'twoPage', 'threePage', 'forPage', 'fifthPage'],
+  animateAnchor: false,
+  scrollBar:true,
+  lazyLoading:false,
+
+  menu: '#mainMenu'});
